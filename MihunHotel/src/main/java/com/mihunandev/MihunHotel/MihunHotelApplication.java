1d37e579d110a38b8d8a57c3ab55e0e33016bc9e
mihunan <mihunanv.21@uom.lk>
2025-02-20 22:47:25 -0800

    Initial commit - Spring Boot Hotel Management System

---
+++ MihunHotel/src/main/java/com/mihunandev/MihunHotel/MihunHotelApplication.java
@@ -0,0 +1,13 @@
+package com.mihunandev.MihunHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MihunHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MihunHotelApplication.class, args);
+	}
+
+}
---
+++ MihunHotel/src/test/java/com/mihunandev/MihunHotel/MihunHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihunandev.MihunHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MihunHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
