43f296c2baabbea32585c9373635333a89a6453a
mihunan <mihunanv.21@uom.lk>
2025-02-21 21:54:45 -0800

    excep,repo

---
+++ MihunHotel/src/main/java/com/mihunandev/MihunHotel/exception/OurException.java
@@ -0,0 +1,8 @@
+package com.mihunandev.MihunHotel.exception;
+
+public class OurException extends RuntimeException {
+
+    public OurException(String message) {
+        super(message);
+    }
+}
---
+++ MihunHotel/src/main/java/com/mihunandev/MihunHotel/repo/BookingRepository.java
@@ -0,0 +1,11 @@
+package com.mihunandev.MihunHotel.repo;
+
+import com.mihunandev.MihunHotel.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
+}
