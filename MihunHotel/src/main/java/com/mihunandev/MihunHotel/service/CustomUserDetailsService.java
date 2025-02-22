9d5b1aca2e4f6463a3843fdb72e021b95063a618
mihunan <mihunanv.21@uom.lk>
2025-02-21 22:26:58 -0800

    security,utils

---
+++ MihunHotel/src/main/java/com/mihunandev/MihunHotel/service/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.mihunandev.MihunHotel.service;
+
+import com.mihunandev.MihunHotel.exception.OurException;
+import com.mihunandev.MihunHotel.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new OurException("Username/Email not Found"));
+    }
+}
